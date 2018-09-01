aa2c675b10d33815901f2ec1ccfbca8d9fda08e3
Nilesh <Nilesh@Nilesh-PC>
2018-09-01 10:12:22 -0400

    Add Hello World code

---
+++ EdurekaProject25082018/src/day1/HalloWorld.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class HalloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("HELLO WORLD AGAIN");
+		System.out.println("MY FIRST JAVA CODE");
+	}
+}
